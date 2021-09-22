fe3a6f32fceb0b29c63723d7dfbc2b9619195ff2
Deyan Romanov <89519509+DeyanRomanov@users.noreply.github.com>
2021-09-22 19:36:35 +0300

    Add files via upload

---
+++ secret_message.py
@@ -0,0 +1,7 @@
+name = input()
+# &quot;Hello, {name}!&quot;. However, Jenny is in love with Johnny
+# and would like to greet him differently: &quot;Hello, my love!&quot;. Can you help her?
+if not name == 'Johnny':
+    print(f'Hello, {name}!')
+else:
+    print('Hello, my love!')
