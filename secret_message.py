bf7544fdb6012bfb777217891b59d31f9fd75e28
Deyan Romanov <89519509+DeyanRomanov@users.noreply.github.com>
2021-09-22 19:38:09 +0300

    Delete secret_message.py

--- secret_message.py
+++
@@ -1,7 +0,0 @@
-name = input()
-# &quot;Hello, {name}!&quot;. However, Jenny is in love with Johnny
-# and would like to greet him differently: &quot;Hello, my love!&quot;. Can you help her?
-if not name == 'Johnny':
-    print(f'Hello, {name}!')
-else:
-    print('Hello, my love!')
