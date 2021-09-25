7dddd7908ab49865dab714f8c672005c9273bcd5
Deyan Romanov <89519509+DeyanRomanov@users.noreply.github.com>
2021-09-25 14:43:44 +0300

    Delete sum_of_chars.py

--- sum_of_chars.py
+++
@@ -1,9 +0,0 @@
-number_of_chars = int(input())
-
-sum_of_chars = 0
-
-for chars in range(number_of_chars):
-    character = input()
-    sum_of_chars += ord(character)
-
-print(f'The sum equals: {sum_of_chars}')
