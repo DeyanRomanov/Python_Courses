cc12018caaff9821440ebd50c87dc06bf325e795
Deyan Romanov <89519509+DeyanRomanov@users.noreply.github.com>
2021-09-27 09:36:32 +0300

    Add files via upload

---
+++ courses.py
@@ -0,0 +1,9 @@
+number_of_courses = int(input())
+
+courses_list = []
+
+for number in range(number_of_courses):
+    courses_name = input()
+    courses_list.append(courses_name)
+
+print(courses_list)
