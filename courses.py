31f846485fc85e753314aebf5f48c52db5b895c0
Deyan Romanov <89519509+DeyanRomanov@users.noreply.github.com>
2021-09-27 09:36:51 +0300

    Delete courses.py

--- courses.py
+++
@@ -1,9 +0,0 @@
-number_of_courses = int(input())
-
-courses_list = []
-
-for number in range(number_of_courses):
-    courses_name = input()
-    courses_list.append(courses_name)
-
-print(courses_list)
