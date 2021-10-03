78d475851a2bc054eaf2f2ba6c73a39d33294f25
Deyan Romanov <89519509+DeyanRomanov@users.noreply.github.com>
2021-10-03 05:02:33 -1200

    Delete min_number_ex.py

--- Fundamentals_SoftUni/min_number_ex.py
+++
@@ -1,9 +0,0 @@
-def smallest_number(one, two, three):
-    return min([one, two, three])
-
-
-number_one = int(input())
-number_two = int(input())
-number_three = int(input())
-
-print(smallest_number(number_one, number_two, number_three))
