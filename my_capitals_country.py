caa41aa6aaeaf0b37e45132beb09d02a047b70c5
Deyan Romanov <89519509+DeyanRomanov@users.noreply.github.com>
2021-10-10 08:26:05 +0300

    Delete my_capitals_country.py

--- my_capitals_country.py
+++
@@ -1,7 +0,0 @@
-countries = input().split(', ')
-capitals = input().split(', ')
-
-countries_dict = dict(zip(countries, capitals))
-
-for key, value in countries_dict.items():
-    print(f'{key} -> {value}')
