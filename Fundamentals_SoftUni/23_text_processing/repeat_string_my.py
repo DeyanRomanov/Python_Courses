7a1b66f4af0a701bfd8456d0cdd75ecb637b14ff
DeyanRomanov <deyan_romanov@gmail.com>
2021-10-13 08:35:18 +0300

    Added new text processing exercises

--- Fundamentals_SoftUni/23_text_processing/repeat_string_my.py
+++
@@ -1,5 +0,0 @@
-string_to_repeat = input().split()
-repeated_string = []
-for i in range(len(string_to_repeat)):
-    repeated_string.append(string_to_repeat[i] * len(string_to_repeat[i]))
-print(''.join(repeated_string))
--- Fundamentals_SoftUni/23_text_processing/text_filter_my.py
+++
@@ -1,7 +0,0 @@
-banned_words = input().split(', ')
-text_to_change = input()
-for word in banned_words:
-    while word in text_to_change:
-        a = '*' * len(word)
-        text_to_change = text_to_change.replace(word, a)
-print(text_to_change)
