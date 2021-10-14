a6d24b3a059fa3f4e3bd5d43e201c713a24aaa5d
Deyan Romanov <89519509+DeyanRomanov@users.noreply.github.com>
2021-10-14 11:26:27 +0300

    Delete match_phone_number_re.py

--- Fundamentals_SoftUni/match_phone_number_re.py
+++
@@ -1,9 +0,0 @@
-import re
-
-text = input()
-pattern = r"\+359(?P<sep>[-\s])2(?P=sep)\d{3}(?P=sep)\d{4}"
-
-result = re.finditer(pattern, text)
-result = [re.group() for re in result]
-
-print(', '.join(result))
