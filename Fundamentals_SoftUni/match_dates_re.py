222b0aabb676c5ab51389d58a9e184c8ecd34821
Deyan Romanov <89519509+DeyanRomanov@users.noreply.github.com>
2021-10-14 11:26:06 +0300

    Delete match_dates_re.py

--- Fundamentals_SoftUni/match_dates_re.py
+++
@@ -1,9 +0,0 @@
-import re
-
-text = input()
-pattern = r"\b(?P<Day>\d{2})(?P<sep>[-/.])(?P<Month>[A-Z]{1}[a-z]{2})(?P=sep)(?P<Year>\d{4})\b"
-
-matches = re.findall(pattern, text)
-for match in matches:
-    print(f"Day: {match[0]}, Month: {match[2]}, Year: {match[3]}")
-
