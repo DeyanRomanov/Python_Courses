16a3cd43c801eac02034c81c2878937a7483d617
Deyan Romanov <89519509+DeyanRomanov@users.noreply.github.com>
2021-10-15 10:10:22 +0300

    Delete extract_email_me.py

--- extract_email_me.py
+++
@@ -1,8 +0,0 @@
-import re
-
-pattern = r"(^|(?<=\s))(?P<user>[A-Za-z0-9]+[-_.A-Za-z0-9]*)@(?P<host>[A-Za-z]+[-A-Za-z]*((\.[A-Za-z])+[A-Za-z]+)+)"
-
-emails = re.finditer(pattern, input())
-
-for email in emails:
-    print(email.group())
