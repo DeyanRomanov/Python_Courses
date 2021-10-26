6f6ee879c9fc4ad5d83e9d229b3bed1062f6ba22
Deyan Romanov <89519509+DeyanRomanov@users.noreply.github.com>
2021-10-26 08:44:07 +0300

    Delete periodic_table.py

--- periodic_table.py
+++
@@ -1,8 +0,0 @@
-numbers_of_elements = int(input())
-
-total_elements = set()
-for _ in range(numbers_of_elements):
-    elements_list = set(input().split())
-    total_elements = total_elements.union(elements_list)
-
-[print(x) for x in total_elements]
