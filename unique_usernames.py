89c9735075059c79f269b3993870a0458446d520
Deyan Romanov <89519509+DeyanRomanov@users.noreply.github.com>
2021-10-26 08:44:24 +0300

    Delete unique_usernames.py

--- unique_usernames.py
+++
@@ -1,9 +0,0 @@
-number_of_usernames = int(input())
-
-usernames = set()
-for _ in range(number_of_usernames):
-    username = input()
-    usernames.add(username)
-
-for username in usernames:
-    print(username)
