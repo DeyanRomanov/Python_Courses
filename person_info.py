dedb7ad198941c4a2f16cfacff071ea259611501
Deyan Romanov <89519509+DeyanRomanov@users.noreply.github.com>
2021-10-31 15:11:52 +0200

    Delete person_info.py

--- person_info.py
+++
@@ -1,6 +0,0 @@
-def get_info(**kwargs):
-    if kwargs['name'] and kwargs['age'] and kwargs['town']:
-        return f"This is {kwargs['name']} from {kwargs['town']} and he is {kwargs['age']} years old"
-
-
-print(get_info(**{"name": "George", "town": "Sofia", "age": 20}))
