4c4f77b3b1103889720a5be46fe66f53d90a3d36
Deyan Romanov <89519509+DeyanRomanov@users.noreply.github.com>
2021-11-01 08:30:57 +0200

    Delete even_or_odd_filtred.py

--- even_or_odd_filtred.py
+++
@@ -1,7 +0,0 @@
-def even_odd(*args):
-    if args[-1] == 'odd':
-        result = list(filter(lambda x: x % 2 != 0, args[:-1]))
-    else:
-        result = list(filter(lambda x: x % 2 == 0, args[:-1]))
-    return result
-
