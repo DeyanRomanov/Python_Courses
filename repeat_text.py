043fea9e0aed534d2d65be8b9efdfbd386c20684
Deyan Romanov <89519509+DeyanRomanov@users.noreply.github.com>
2021-11-05 18:16:22 -1200

    Delete repeat_text.py

--- repeat_text.py
+++
@@ -1,8 +0,0 @@
-# "Variable times must be an integer"
-
-try:
-    text_to_repeat = input()
-    time_to_repeat = int(input())
-    print(text_to_repeat * time_to_repeat)
-except ValueError:
-    print("Variable times must be an integer")
