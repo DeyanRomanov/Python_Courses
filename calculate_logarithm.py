223ba41514bda015d8864ee4af10a6e555e3eb4d
Deyan Romanov <89519509+DeyanRomanov@users.noreply.github.com>
2021-11-11 08:21:46 +0200

    Add files via upload

---
+++ calculate_logarithm.py
@@ -0,0 +1,6 @@
+from test_modules.test_module import logarithm
+
+number = int(input())
+base = input()
+
+print(logarithm(number, base))
---
+++ math_operations.py
@@ -0,0 +1,7 @@
+from test_modules.mathematical_operations import mathematics_operation
+
+number_one = input()
+operator = input()
+number_two = input()
+
+print(mathematics_operation(number_one, operator, number_two))
