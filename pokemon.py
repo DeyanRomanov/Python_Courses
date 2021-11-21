8ec822653213dd6307c281a44b2bdd339dd20e69
Deyan Romanov <89519509+DeyanRomanov@users.noreply.github.com>
2021-11-21 18:57:56 +0200

    Delete pokemon.py

--- pokemon.py
+++
@@ -1,8 +0,0 @@
-class Pokemon:
-    def __init__(self, names, health):
-        self.names = names
-        self.health = health
-
-    def pokemon_details(self):
-        return f"{self.names} with health {self.health}"
-
