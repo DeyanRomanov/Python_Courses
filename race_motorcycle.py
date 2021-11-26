e6f33a49e0b2641b3b75e324c719ecd8eb40d932
Deyan Romanov <89519509+DeyanRomanov@users.noreply.github.com>
2021-11-26 09:29:05 +0200

    Delete race_motorcycle.py

--- race_motorcycle.py
+++
@@ -1,9 +0,0 @@
-from project_need_for_speed.motorcycle import Motorcycle
-
-
-class RaceMotorcycle(Motorcycle):
-    DEFAULT_FUEL_CONSUMPTION = 8
-
-    def __init__(self, fuel: float, horse_power: int):
-        super().__init__(fuel, horse_power)
-
