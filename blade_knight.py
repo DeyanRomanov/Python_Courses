2f6451d75f034ef3883b6da428ec33b090a04137
Deyan Romanov <89519509+DeyanRomanov@users.noreply.github.com>
2021-11-26 09:25:33 +0200

    Delete blade_knight.py

--- blade_knight.py
+++
@@ -1,7 +0,0 @@
-from project_hero.dark_knight import DarkKnight
-
-
-class BladeKnight(DarkKnight):
-
-    def __init__(self, username: str, level: int):
-        DarkKnight.__init__(self, username, level)
