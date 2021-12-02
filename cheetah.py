aca2681c0dff3723f495c3df5057eabea846584a
Deyan Romanov <89519509+DeyanRomanov@users.noreply.github.com>
2021-12-02 11:52:15 +0200

    Delete cheetah.py

--- cheetah.py
+++
@@ -1,9 +0,0 @@
-from project_zoo.animal import Animal
-
-
-class Cheetah(Animal):
-    MONEY_FOR_CARE = 60
-
-    def __init__(self, name, age, gender):
-        super().__init__(name, gender, age, Cheetah.MONEY_FOR_CARE)
-
