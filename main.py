abde32a66c9f8b4eb040a970c96e1ef68664354a
Deyan Romanov <89519509+DeyanRomanov@users.noreply.github.com>
2021-12-08 16:37:22 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+from project_photo_album.photo_album import PhotoAlbum
+
+album = PhotoAlbum(2)
+
+print(album.add_photo("baby"))
+print(album.add_photo("first grade"))
+print(album.add_photo("eight grade"))
+print(album.add_photo("party with friends"))
+print(album.photos)
+print(album.add_photo("prom"))
+print(album.add_photo("wedding"))
+
+print(album.display())
