17e0ffb37c8b6dbbd1032b1deb2e6d7c9e15b829
Deyan Romanov <89519509+DeyanRomanov@users.noreply.github.com>
2021-12-23 09:58:07 +0200

    Add files via upload

---
+++ inside_table.py
@@ -0,0 +1,17 @@
+from exam_prepare_second.project.table.table import Table
+
+
+class InsideTable(Table):
+
+    def __init__(self, table_number, capacity):
+        super().__init__(table_number, capacity)
+        
+    @property
+    def capacity(self):
+        return self.__capacity
+
+    @capacity.setter
+    def capacity(self, value):
+        if value < 1 or value > 50:
+            raise ValueError("Inside table's number must be between 1 and 50 inclusive!")
+        self.__capacity = value
