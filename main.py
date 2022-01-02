956adc8f7fc9844e760a9e7020e69d7ef1738f93
Deyan Romanov <89519509+DeyanRomanov@users.noreply.github.com>
2022-01-02 11:53:15 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,152 +1,133 @@
-from project_storage.customer import Customer
-from project_storage.dvd import DVD
-from project_storage.movie_world import MovieWorld
+from project_storage.category import Category
+from project_storage.document import Document
+from project_storage.storage import Storage
+from project_storage.topic import Topic
 
 import unittest
 
 
-class TestsMovieWorld(unittest.TestCase):
-    def test_customer_init(self):
-        c = Customer("Pesho", 22, 1)
-        self.assertEqual(c.name, "Pesho")
-        self.assertEqual(c.age, 22)
-        self.assertEqual(c.id, 1)
+class TestDocumentManagement(unittest.TestCase):
+    def setUp(self):
+        self.c = Category(1, "C")
+        self.t = Topic(1, "T", "C:\\user")
+        self.d = Document(1, 1, 1, "D")
+        self.s = Storage()
 
-    def test_customer_repr(self):
-        c = Customer("Pesho", 22, 1)
-        self.assertEqual(repr(c), "1: Pesho of age 22 has 0 rented DVD's ()")
+    def test_category_init(self):
+        self.assertEqual(self.c.id, 1)
+        self.assertEqual(self.c.name, "C")
 
-    def test_dvd_init(self):
-        dvd = DVD("B", 1, 2020, "January", 10)
-        self.assertEqual(dvd.name, "B")
-        self.assertEqual(dvd.id, 1)
-        self.assertEqual(dvd.creation_month, "January")
-        self.assertEqual(dvd.creation_year, 2020)
-        self.assertEqual(dvd.age_restriction, 10)
-        self.assertEqual(dvd.is_rented, False)
+    def test_category_edit(self):
+        self.c.edit("new")
+        self.assertEqual(self.c.name, "new")
 
-    def test_dvd_class_method(self):
-        dvd = DVD.from_date(1, "A", "16.10.1997", 18)
-        self.assertEqual(dvd.name, "A")
-        self.assertEqual(dvd.id, 1)
-        self.assertEqual(dvd.creation_month, "October")
-        self.assertEqual(dvd.creation_year, 1997)
-        self.assertEqual(dvd.age_restriction, 18)
-        self.assertEqual(dvd.is_rented, False)
+    def test_category_repr(self):
+        self.assertEqual(str(self.c), "Category 1: C")
 
-    def test_dvd_repr(self):
-        dvd = DVD.from_date(1, "A", "16.10.1997", 18)
-        self.assertEqual(repr(dvd), "1: A (October 1997) has age restriction 18. Status: not rented")
+    def test_document_init(self):
+        self.assertEqual(self.d.id, 1)
+        self.assertEqual(self.d.category_id, 1)
+        self.assertEqual(self.d.topic_id, 1)
+        self.assertEqual(self.d.file_name, "D")
+        self.assertEqual(self.d.tags, [])
 
-    def test_movie_init(self):
-        movie = MovieWorld("Test")
-        self.assertEqual(movie.name, "Test")
-        self.assertEqual(movie.customers, [])
-        self.assertEqual(movie.dvds, [])
+    def test_document_from_intsances(self):
+        doc = Document.from_instances(1, self.c, self.t, "Doc")
+        self.assertEqual(doc.id, 1)
+        self.assertEqual(doc.category_id, 1)
+        self.assertEqual(doc.topic_id, 1)
+        self.assertEqual(doc.file_name, "Doc")
+        self.assertEqual(doc.tags, [])
 
-    def tests_movie_world_dvd_capacity(self):
-        self.assertEqual(MovieWorld.dvd_capacity(), 15)
+    def test_document_add_tag(self):
+        self.d.add_tag("tag")
+        self.d.add_tag("tag")
+        self.assertEqual(self.d.tags, ["tag"])
 
-    def tests_movie_world_customer_capacity(self):
-        self.assertEqual(MovieWorld.customer_capacity(), 10)
+    def test_document_remove_tag(self):
+        self.d.add_tag("tag")
+        self.d.add_tag("tag")
+        self.d.remove_tag("tag")
+        self.assertEqual(self.d.tags, [])
 
-    def test_movie_world_add_customer_success(self):
-        movie_world = MovieWorld("Test")
-        c = Customer("Pesho", 20, 4)
-        movie_world.add_customer(c)
-        self.assertEqual(movie_world.customers, [c])
+    def test_document_edit(self):
+        self.d.edit("new")
+        self.assertEqual(self.d.file_name, "new")
 
-    def test_movie_world_add_customer_overflow(self):
-        movie_world = MovieWorld("Test")
-        for _ in range(11):
-            movie_world.add_customer(Customer("Pesho", 20, 4))
-        self.assertEqual(len(movie_world.customers), 10)
+    def test_document_repr(self):
+        self.d.add_tag("tag")
+        self.assertEqual(str(self.d), 'Document 1: D; category 1, topic 1, tags: tag')
 
-    def test_movie_world_add_dvd_success(self):
-        movie_world = MovieWorld("Test")
-        d = DVD("A", 1, 1254, "February", 10)
-        movie_world.add_dvd(d)
-        self.assertEqual(movie_world.dvds, [d])
+    def test_topic_init(self):
+        self.assertEqual(self.t.id, 1)
+        self.assertEqual(self.t.id, 1)
+        self.assertEqual(self.t.storage_folder, "C:\\user")
 
-    def test_movie_world_add_dvd_overflow(self):
-        movie_world = MovieWorld("Test")
-        for _ in range(16):
-            movie_world.add_dvd(DVD("A", 1, 1254, "February", 10))
-        self.assertEqual(len(movie_world.dvds), 15)
+    def test_topic_edit(self):
+        self.t.edit("new topic", "new folder")
+        self.assertEqual(self.t.topic, "new topic")
+        self.assertEqual(self.t.storage_folder, "new folder")
 
-    def test_movie_world_rent_dvd_already_rented(self):
-        movie_world = MovieWorld("Test")
-        d = DVD("A", 1, 1254, "February", 10)
-        c = Customer("Pesho", 20, 4)
-        c2 = Customer("Gosho", 26, 2)
-        movie_world.add_customer(c)
-        movie_world.add_customer(c2)
-        movie_world.add_dvd(d)
-        movie_world.rent_dvd(4, 1)
-        result = movie_world.rent_dvd(2, 1)
-        self.assertEqual(result, "DVD is already rented")
+    def test_topic_repr(self):
+        self.assertEqual(str(self.t), "Topic 1: T in C:\\user")
 
-    def test_movie_world_rent_dvd_already_rented_by_user(self):
-        movie_world = MovieWorld("Test")
-        d = DVD("A", 1, 1254, "February", 10)
-        c = Customer("Pesho", 20, 4)
-        movie_world.add_customer(c)
-        movie_world.add_dvd(d)
-        movie_world.rent_dvd(4, 1)
-        result = movie_world.rent_dvd(4, 1)
-        self.assertEqual(result, "Pesho has already rented A")
+    def test_storage_init(self):
+        self.assertEqual(self.s.categories, [])
+        self.assertEqual(self.s.topics, [])
+        self.assertEqual(self.s.documents, [])
 
-    def test_movie_world_rent_dvd_no_permision(self):
-        movie_world = MovieWorld("Test")
-        d = DVD("A", 1, 1254, "February", 18)
-        c = Customer("Pesho", 16, 4)
-        movie_world.add_customer(c)
-        movie_world.add_dvd(d)
-        result = movie_world.rent_dvd(4, 1)
-        self.assertEqual(result, "Pesho should be at least 18 to rent this movie")
+    def test_storage_add_category(self):
+        self.s.add_category(self.c)
+        self.s.add_category(self.c)
+        self.assertEqual(self.s.categories, [self.c])
 
-    def test_movie_world_rent_dvd_success(self):
-        movie_world = MovieWorld("Test")
-        d = DVD("A", 1, 1254, "February", 10)
-        c = Customer("Pesho", 20, 4)
-        movie_world.add_customer(c)
-        movie_world.add_dvd(d)
-        result = movie_world.rent_dvd(4, 1)
-        self.assertEqual(result, "Pesho has successfully rented A")
-        self.assertEqual(d.is_rented, True)
-        self.assertEqual(c.rented_dvds[0], d)
+    def test_storage_add_topic(self):
+        self.s.add_topic(self.t)
+        self.s.add_topic(self.t)
+        self.assertEqual(self.s.topics, [self.t])
 
-    def test_movie_world_return_dvd_success(self):
-        movie_world = MovieWorld("Test")
-        d = DVD("A", 1, 1254, "February", 10)
-        c = Customer("Pesho", 20, 4)
-        movie_world.add_customer(c)
-        movie_world.add_dvd(d)
-        movie_world.rent_dvd(4, 1)
-        result = movie_world.return_dvd(4, 1)
-        self.assertEqual(result, "Pesho has successfully returned A")
-        self.assertEqual(c.rented_dvds, [])
-        self.assertEqual(d.is_rented, False)
+    def test_storage_add_document(self):
+        self.s.add_document(self.d)
+        self.s.add_document(self.d)
+        self.assertEqual(self.s.documents, [self.d])
 
-    def test_movie_world_return_dvd_unsuccessful(self):
-        movie_world = MovieWorld("Test")
-        d = DVD("A", 1, 1254, "February", 10)
-        c = Customer("Pesho", 20, 4)
-        movie_world.add_customer(c)
-        movie_world.add_dvd(d)
-        result = movie_world.return_dvd(4, 1)
-        self.assertEqual(result, "Pesho does not have that DVD")
+    def test_storage_edit_category(self):
+        self.s.add_category(self.c)
+        self.s.edit_category(1, "new")
+        self.assertEqual(self.s.categories[0].name, "new")
 
-    def test_movie_world_repr(self):
-        movie_world = MovieWorld("Test")
-        d = DVD("A", 1, 1254, "February", 10)
-        c = Customer("Pesho", 20, 4)
-        movie_world.add_customer(c)
-        movie_world.add_dvd(d)
-        movie_world.rent_dvd(4, 1)
-        actual = repr(movie_world).strip('\n')
-        expected = "4: Pesho of age 20 has 1 rented DVD's (A)\n1: A (February 1254) has age restriction 10. Status: rented"
-        self.assertEqual(actual, expected)
+    def test_storage_edit_topic(self):
+        self.s.add_topic(self.t)
+        self.s.edit_topic(1, "new", "new storage")
+        self.assertEqual(self.s.topics[0].topic, "new")
+        self.assertEqual(self.s.topics[0].storage_folder, "new storage")
+
+    def test_storage_edit_document(self):
+        self.s.add_document(self.d)
+        self.s.edit_document(1, "new")
+        self.assertEqual(self.s.documents[0].file_name, "new")
+
+    def test_storage_delete_category(self):
+        self.s.add_category(self.c)
+        self.s.delete_category(1)
+        self.assertEqual(self.s.categories, [])
+
+    def test_storage_delete_topic(self):
+        self.s.add_topic(self.t)
+        self.s.delete_topic(1)
+        self.assertEqual(self.s.topics, [])
+
+    def test_storage_delete_document(self):
+        self.s.add_document(self.d)
+        self.s.delete_document(1)
+        self.assertEqual(self.s.documents, [])
+
+    def test_storage_repr(self):
+        self.s.add_category(self.c)
+        self.s.add_topic(self.t)
+        self.s.add_document(self.d)
+        expected = str(self.s).strip('\n')
+        self.assertEqual(expected, "Document 1: D; category 1, topic 1, tags: ")
 
 
 if __name__ == "__main__":
