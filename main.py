ce92b21bdbfa029dbe5037ca91624c3db2fe6354
Deyan Romanov <89519509+DeyanRomanov@users.noreply.github.com>
2022-01-04 19:21:49 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
-from project_animal.cat import Cat
-from project_animal.dog import Dog
-from project_animal.kitten import Kitten
-from project_animal.tomcat import Tomcat
+# first zero test
+import unittest
+from project_wild_farm.animals.birds import Owl, Hen
+from project_wild_farm.animals.mammals import Mouse, Dog, Cat, Tiger
+from project_wild_farm.food import Vegetable, Fruit, Meat, Seed
 
-dog = Dog("Rocky", 3, "Male")
-print(dog.make_sound())
-print(dog)
-tomcat = Tomcat("Tom", 6)
-print(tomcat.make_sound())
-print(tomcat)
-kitten = Kitten("Kiki", 1)
-print(kitten.make_sound())
-print(kitten)
-cat = Cat("Johnny", 7, "Male")
-print(cat.make_sound())
-print(cat)
+class WildFarmTests(unittest.TestCase):
+    def test_first_zero(self):
+        owl = Owl("Pip", 10, 10)
+        self.assertEqual(str(owl), "Owl [Pip, 10, 10, 0]")
+        meat = Meat(4)
+        self.assertEqual(owl.make_sound(), "Hoot Hoot")
+        owl.feed(meat)
+        veg = Vegetable(1)
+        owl.feed(veg)
+        self.assertEqual(owl.feed(veg), "Owl does not eat Vegetable!")
+        self.assertEqual(str(owl), "Owl [Pip, 10, 11.0, 4]")
+
+if __name__ == "__main__":
+    unittest.main()
