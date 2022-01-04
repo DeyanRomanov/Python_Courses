6eccfd6379df96a52ffe369e367d486afa2f2b9f
Deyan Romanov <89519509+DeyanRomanov@users.noreply.github.com>
2022-01-04 19:21:32 +0200

    Add files via upload

--- animal.py
+++ animal.py
@@ -1,15 +1,43 @@
 from abc import ABC, abstractmethod
+from project_wild_farm.food import Food
 
 
 class Animal(ABC):
-    def __init__(self, name, age, gender):
+    FOOD_TYPE = []
+    WEIGHT_MULTIPLIER = 1
+
+    def __init__(self, name, weight):
         self.name = name
-        self.age = age
-        self.gender = gender
+        self.weight = weight
+        self.food_eaten = 0
 
     @abstractmethod
     def make_sound(self):
         pass
 
+    def feed(self, food: Food):
+        food_name = food.__class__.__name__
+        if food_name not in self.FOOD_TYPE:
+            return f"{self.__class__.__name__} does not eat {food_name}!"
+        self.weight += food.quantity * self.WEIGHT_MULTIPLIER
+        self.food_eaten += food.quantity
+
+
+class Bird(Animal, ABC):
+    @abstractmethod
+    def __init__(self, name, weight, wing_size):
+        super().__init__(name, weight)
+        self.wing_size = wing_size
+
     def __repr__(self):
-        return f"This is {self.name}. {self.name} is a {self.age} year old {self.gender} {self.__class__.__name__}"
+        return f"{self.__class__.__name__} [{self.name}, {self.wing_size}, {self.weight}, {self.food_eaten}]"
+
+
+class Mammals(Animal, ABC):
+    @abstractmethod
+    def __init__(self, name, weight, living_region):
+        super().__init__(name, weight)
+        self.living_region = living_region
+
+    def __repr__(self):
+        return f"{self.__class__.__name__} [{self.name}, {self.weight}, {self.living_region}, {self.food_eaten}]"
