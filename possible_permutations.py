37a7ef8de3edfe6c9e9f901f2b9a5ae8104346cb
Deyan Romanov <89519509+DeyanRomanov@users.noreply.github.com>
2022-01-09 19:42:44 -1200

    Delete possible_permutations.py

--- possible_permutations.py
+++
@@ -1,9 +0,0 @@
-import itertools
-
-
-def possible_permutations(nums):
-    for perm in list(itertools.permutations(nums)):
-        yield list(perm)
-
-
-[print(n) for n in possible_permutations([1, 2, 3])]
