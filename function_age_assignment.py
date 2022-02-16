8850fb7deb6340cde0d8522747cc3cb2c1f4b1b5
Deyan Romanov <89519509+DeyanRomanov@users.noreply.github.com>
2022-02-15 18:32:35 -1200

    Delete function_age_assignment.py

--- function_age_assignment.py
+++
@@ -1,8 +0,0 @@
-def age_assignment(*args, **kwargs):
-    new_dict = {}
-    for names in args:
-        for key, value in kwargs.items():
-            if key == names[0]:
-                new_dict[names] = value
-    return new_dict
-
