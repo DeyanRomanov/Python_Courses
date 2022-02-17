1db619b639f3b329740af57073961d7be1b48864
Deyan Romanov <89519509+DeyanRomanov@users.noreply.github.com>
2022-02-17 08:26:01 +0200

    Delete flatten_lists.py

--- flatten_lists.py
+++
@@ -1,8 +0,0 @@
-string_with_numbers = input().split('|')
-
-matrix = []
-reverse_result = []
-for x in range(len(string_with_numbers) - 1, -1, -1):
-    reverse_result += string_with_numbers[x].split()
-
-print(' '.join(reverse_result))
