e004f546cfa5375770f9ebbf83de149ccdadf682
Deyan Romanov <89519509+DeyanRomanov@users.noreply.github.com>
2022-06-12 18:11:49 +0300

    Add files via upload

---
+++ arithmetic_operators.py
@@ -0,0 +1,9 @@
+a = int(input())
+b = int(input())
+
+sum_of_numbers = a + b
+difference_of_numbers = a - b
+product_of_numbers = a * b
+print(sum_of_numbers)
+print(difference_of_numbers)
+print(product_of_numbers)
