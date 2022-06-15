e46590f3672a2bf11b76c3e3a5367892a42bb13c
Deyan Romanov <89519509+DeyanRomanov@users.noreply.github.com>
2022-06-15 09:14:11 +0300

    Delete arithmetic_operators.py

--- arithmetic_operators.py
+++
@@ -1,9 +0,0 @@
-a = int(input())
-b = int(input())
-
-sum_of_numbers = a + b
-difference_of_numbers = a - b
-product_of_numbers = a * b
-print(sum_of_numbers)
-print(difference_of_numbers)
-print(product_of_numbers)
